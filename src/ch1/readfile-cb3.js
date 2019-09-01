b59ab12ff50899302f4f6ef231df2a499c08eb7e
takuyanin <ftakuyamax0701@gmail.com>
2019-09-02 06:17:39 +0900

    Add sleep bofore asynced function

--- src/ch1/readfile-cb3.js
+++ src/ch1/readfile-cb3.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
+const sleep = require('sleep')
 
 fs.readFile('a.txt', 'utf-8', function (err, data) {
   console.log('a.txtを読みました', data)
+  sleep.sleep(1)
   fs.readFile('b.txt', 'utf-8', function (err, data) {
+    sleep.sleep(1)
     console.log('b.txtを読みました', data)
     fs.readFile('c.txt', 'utf-8', function (err, data) {
+      sleep.sleep(1)
       console.log('c.txtを読みました', data)
     })
   })
 })
-
