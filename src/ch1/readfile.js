f78f50c04f2223a0447d1abaaddd9d0688cf45df
takuyanin <ftakuyamax0701@gmail.com>
2019-09-02 04:10:28 +0900

    fix indent

--- src/ch1/readfile.js
+++ src/ch1/readfile.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 fs.readFile('kakugen.txt', 'utf-8', kakugenLoaded)
 
 function kakugenLoaded (err, data) {
-    if (err) {
-        console.log('読み込みに失敗')
-    }
-    console.log(data)
+  if (err) {
+    console.log('読み込みに失敗')
+  }
+  console.log(data)
 }
