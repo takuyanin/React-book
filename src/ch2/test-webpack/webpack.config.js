44ad1bb13464f4e7b438ff08a2a6600ae2d61094
takuyanin <ftakuyamax0701@gmail.com>
2019-09-04 12:20:50 +0900

    Start and build a project with Webpack

---
+++ src/ch2/test-webpack/webpack.config.js
@@ -0,0 +1,18 @@
+﻿module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: __dirname + '/out',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  }
+}
