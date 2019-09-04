74f6c42d3c1e1d54ed1a0c4bcecc27cbacd3a27a
takuyanin <ftakuyamax0701@gmail.com>
2019-09-05 04:39:57 +0900

    Fix mainly css things

---
+++ src/ch3/stopwatch/webpack.config.js
@@ -0,0 +1,22 @@
+﻿module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: __dirname + '/out',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react']
+        }
+      },
+      {
+        test: /.css$/,
+        loader: 'css-loader',
+      }
+    ],
+  }
+}
