2d5c2c00aa5a95263a9c59dfdf11384775426b00
takuyanin <ftakuyamax0701@gmail.com>
2019-09-09 13:56:17 +0900

    Add a dir for form input

---
+++ src/ch3/form_simple/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SimpleForm from './SimpleForm'
+// import 'App.css'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <SimpleForm />
+      </div>
+    )
+  }
+}
